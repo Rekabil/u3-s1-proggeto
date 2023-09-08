471a0debcda443209d1ff49d9d9c8c2afb13ae93
Bilgen Kaan Remzi <137759282+Rekabil@users.noreply.github.com>
2023-09-08 17:29:09 +0200

    Container fix

--- src/component/MyNav.jsx
+++ src/component/MyNav.jsx
@@ -1,34 +1,36 @@
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav, Container } from "react-bootstrap";
 import logo from "../assets/logo.png";
 
 const MyNav = () => {
   return (
-    <Navbar expand="lg" className="align-content-center">
-      <Navbar.Brand href="#home" className="m-0">
-        <img src={logo} alt="" height={75} />
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" className="btn btn-outline-light" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#home">TV Shows</Nav.Link>
-          <Nav.Link href="#home">Movies</Nav.Link>
-          <Nav.Link href="#home">Recently Added</Nav.Link>
-          <Nav.Link href="#home">My List</Nav.Link>
-        </Nav>
-        <a href="#search">
-          <i className="bi bi-search icons"></i>
-        </a>
-        <a href="#kids" id="kids" className="icons">
-          KIDS
-        </a>
-        <a href="#search">
-          <i className="bi bi-bell-fill icons"></i>
-        </a>
-        <a href="#search">
-          <i className="bi bi-person-fill icons"></i>
-        </a>
-      </Navbar.Collapse>
+    <Navbar variant="dark" expand="lg" className="align-content-center">
+      <Container fluid>
+        <Navbar.Brand href="#home" className="m-0">
+          <img src={logo} alt="" height={75} />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" className="btn btn-outline-light" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#home">TV Shows</Nav.Link>
+            <Nav.Link href="#home">Movies</Nav.Link>
+            <Nav.Link href="#home">Recently Added</Nav.Link>
+            <Nav.Link href="#home">My List</Nav.Link>
+          </Nav>
+          <a href="#search">
+            <i className="bi bi-search icons"></i>
+          </a>
+          <a href="#kids" id="kids" className="icons">
+            KIDS
+          </a>
+          <a href="#search">
+            <i className="bi bi-bell-fill icons"></i>
+          </a>
+          <a href="#search">
+            <i className="bi bi-person-fill icons"></i>
+          </a>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 };
